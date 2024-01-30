081d2e010dcedcdb5f825bee47141dbec2666673
Pratiksharai-rumsan <pratiksha.rai@rumsan.net>
2024-01-30 13:53:55 +0545

    feat:setup nest ionic app for navigation

---
+++ myTabs/src/pages/Tabs2.tsx
@@ -0,0 +1,16 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react"
+
+const Tabs2:React.FC =()=>{
+    return (
+        <IonPage>
+<IonHeader>
+    <IonToolbar>
+        <IonTitle>Tabs</IonTitle>
+    </IonToolbar>
+</IonHeader>
+<IonContent></IonContent>
+</IonPage>
+    )
+}
+
+export default Tabs2
